c0651d10160000d6c87d93bb073dce83b7a5b914
enzoggqs <eggqsaraiva@gmail.com>
2023-08-14 18:34:20 -0300

    Create CustomInput Component

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,51 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { Flex } from "@chakra-ui/layout";
-import { Text, Divider, Input, InputLeftElement, InputGroup, InputRightElement, Button, FormControl, FormLabel, FormErrorMessage } from "@chakra-ui/react";
-import { EmailIcon, LockIcon, ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-import { Field, Form, Formik } from 'formik';
+import { Text, Divider, Button } from "@chakra-ui/react";
+import { EmailIcon, LockIcon } from '@chakra-ui/icons';
+import { Form, Formik } from 'formik';
 import * as Yup from "yup";
 import Auth from "../../services/Auth.js";
 import { useNavigate } from "react-router-dom";
-
-const CustomInput = ({ icon, label, type, show, handleClick, touched, errors,...props }) => (
-  <Field name={props.name}>
-    {({ field, form }) => (
-      <FormControl
-        isInvalid={form.errors[field.name] && form.touched[field.name]}
-        pb="4"
-      >
-        <FormLabel 
-          fontSize="xl"
-          color="primary.600"
-          mt=".5rem"
-          fontWeight="medium"
-        >
-          {label}
-        </FormLabel>
-        <InputGroup>
-          <InputLeftElement ml=".5rem" h="full" pointerEvents="none">
-            {icon}
-          </InputLeftElement>
-          <Input
-            {...field}
-            {...props}
-            type={type === "password" ? (show ? "text" : "password") : type}
-          />
-          {type === "password" && (
-            <InputRightElement h="full" width="4.5rem">
-              <Button h="1.75rem" size="sm" onClick={handleClick}>
-                {show ? <ViewOffIcon /> : <ViewIcon />}
-              </Button>
-            </InputRightElement>
-          )}
-        </InputGroup>
-        <FormErrorMessage>{form.errors[field.name]}</FormErrorMessage>
-      </FormControl>
-    )}
-  </Field>
-);
-
-
+import CustomInput from '../../components/CustomInput/index.js';
 
 const Login = () => {
   const navigate = useNavigate();
