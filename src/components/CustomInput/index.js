2c78e967a44c89f826913f8af5a2529d5688539f
enzoggqs <eggqsaraiva@gmail.com>
2023-08-15 14:15:43 -0300

    Style CustomInput component

--- src/components/CustomInput/index.js
+++ src/components/CustomInput/index.js
@@ -2,43 +2,46 @@ import React from 'react'
 import { Input, InputLeftElement, InputGroup, InputRightElement, Button, FormControl, FormLabel, FormErrorMessage } from "@chakra-ui/react";
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 import { Field } from 'formik';
-
-const CustomInput = ({ icon, label, type, show, handleClick, touched, errors,...props }) => (
-  <Field name={props.name}>
-    {({ field, form }) => (
-      <FormControl
-        isInvalid={form.errors[field.name] && form.touched[field.name]}
-        pb="4"
-      >
-        <FormLabel 
-          fontSize="xl"
-          color="primary.600"
-          mt=".5rem"
-          fontWeight="medium"
+import "./customInput.css";
+  
+const CustomInput = ({ icon, label, type, show, handleClick, touched, errors,...props }) => {
+  return (
+    <Field name={props.name}>
+      {({ field, form }) => (
+        <FormControl
+          isInvalid={form.errors[field.name] && form.touched[field.name]}
+          pb="4"
         >
-          {label}
-        </FormLabel>
-        <InputGroup>
-          <InputLeftElement ml=".5rem" h="full" pointerEvents="none">
-            {icon}
-          </InputLeftElement>
-          <Input
-            {...field}
-            {...props}
-            type={type === "password" ? (show ? "text" : "password") : type}
-          />
-          {type === "password" && (
-            <InputRightElement h="full" width="4.5rem">
-              <Button h="1.75rem" size="sm" onClick={handleClick}>
-                {show ? <ViewOffIcon /> : <ViewIcon />}
-              </Button>
-            </InputRightElement>
-          )}
-        </InputGroup>
-        <FormErrorMessage>{form.errors[field.name]}</FormErrorMessage>
-      </FormControl>
-    )}
-  </Field>
-);
+          <FormLabel 
+            fontSize="xl"
+            color="primary.600"
+            mt=".5rem"
+            fontWeight="medium"
+          >
+            {label}
+          </FormLabel>
+          <InputGroup>
+            <InputLeftElement ml=".5rem" h="full" pointerEvents="none">
+              {icon}
+            </InputLeftElement>
+            <Input
+              {...field}
+              {...props}
+              type={type === "password" ? (show ? "text" : "password") : type}
+            />
+            {type === "password" && (
+              <InputRightElement h="full" width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleClick}>
+                  {show ? <ViewOffIcon /> : <ViewIcon />}
+                </Button>
+              </InputRightElement>
+            )}
+          </InputGroup>
+          <FormErrorMessage>{form.errors[field.name]}</FormErrorMessage>
+        </FormControl>
+      )}
+    </Field>
+  )
+}
 
 export default CustomInput
