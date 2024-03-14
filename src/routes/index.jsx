934544955ade83303880f0f5f5ffe39becaeea28
Enzo Gabriel Guedes <eggqsaraiva@gmail.com>
2024-03-13 23:05:21 -0300

    Update routes file

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,15 +3,25 @@ import PathRoutes from "./PathRoutes";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Home from "../pages/Home";
+import Profile from "../pages/Profile";
+import { Flex } from "@chakra-ui/react";
 
 const RoutesComponent = () => {
   return (
-    <Routes>
-      <Route exect path={PathRoutes.LOGIN} element={<Login />} />
-
-      <Route exect path={PathRoutes.REGISTER} element={<Register />} />
-      <Route exect path={PathRoutes.HOME} element={<Home />} />
-    </Routes>
+    <Flex
+      backgroundColor="primary.500"
+      alignItems="center"
+      justifyContent="center"
+      h={"100vh"}
+      w={"100vw"}
+    >
+      <Routes>
+          <Route exect path={PathRoutes.REGISTER} element={<Register />} />
+          <Route exect path={PathRoutes.LOGIN} element={<Login />} />
+          <Route exect path={PathRoutes.HOME} element={<Home />} />
+          <Route exect path={PathRoutes.PROFILE} element={<Profile />} />
+      </Routes>
+    </Flex>
   )
 }
 
