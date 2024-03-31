361adb29e82a6b6a362188e6d333196d2c9a57bc
Enzo Gabriel Guedes <eggqsaraiva@gmail.com>
2024-03-31 16:25:25 -0300

    pass user context to the entire application

--- src/index.jsx
+++ src/index.jsx
@@ -6,27 +6,30 @@ import Routes from "./routes";
 import themeVariables from "./config/Theme";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { UserProvider } from "./context/userContext";
 
 const theme = extendTheme(themeVariables);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <Routes />
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </BrowserRouter>
-    </ChakraProvider>
+    <UserProvider>
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Routes />
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </BrowserRouter>
+      </ChakraProvider>
+    </UserProvider>
   </React.StrictMode>
 );
