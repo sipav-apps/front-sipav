4b9f158c8177e2a1f591681dddce447cd5b858c0
Enzo Gabriel Guedes <eggqsaraiva@gmail.com>
2024-03-31 16:21:04 -0300

    Add fontWeights to Theme.js file

--- src/config/Theme.js
+++ src/config/Theme.js
@@ -12,6 +12,18 @@ const theme = {
     heading: "Montserrat, sans-serif",
     body: "Montserrat, sans-serif",
   },
+  fontWeights: {
+    hairline: 100,
+    thin: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    soso: 550,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+  },
 };
 
 export default theme;
