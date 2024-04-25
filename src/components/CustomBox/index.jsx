6049997c289dc49e5f948544f17d2ef562602e06
Enzo Gabriel Guedes <eggqsaraiva@gmail.com>
2024-04-25 16:00:40 -0300

    Update image size in CustomBox component

--- src/components/CustomBox/index.jsx
+++ src/components/CustomBox/index.jsx
@@ -4,26 +4,27 @@ import React from 'react'
 const CustomBox = ({text, rightImage}) => {
   return (
     <Flex 
-        width="100%"
-        justifyContent="space-between"
-        align="center"
-        borderWidth=".1rem"
-        borderRadius="15px"
-        py=".5rem"
-        px="1rem"
-        my="1rem"
-        sx={{ boxShadow: '0px 0px 16px 1px grey'}}
+      width="100%"
+      justifyContent="space-between"
+      align="center"
+      borderWidth=".1rem"
+      borderRadius="15px"
+      py=".5rem"
+      px="1rem"
+      my="1rem"
+      sx={{ boxShadow: '0px 0px 16px 1px grey'}}
     >
-        <Text 
-            fontSize="md" 
-            fontWeight="semibold" 
-            color="primary.600"
-
-            px="10px"
-        >
-            {text}
-        </Text>
+      <Text 
+          fontSize="md" 
+          fontWeight="semibold" 
+          color="primary.600"
+          px="10px"
+      >
+        {text}
+      </Text>
+      <Flex minWidth="30px" alignItems="center">
         {rightImage}
+      </Flex>
     </Flex>
   )
 }
